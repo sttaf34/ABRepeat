212eff730b3d3481900cdab0b74eb98ad671fb6a
sttaf34 <sttaf34@gmail.com>
2014-04-27 02:38:53 +0900

    Initial commit

---
+++ ABRepeat/Classes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ABRepeat
+//
+//  Created by Shinichi Kawamura on 4/27/14.
+//  Copyright (c) 2014 Shinichi Kawamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ABRepeat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ABRepeat
+//
+//  Created by Shinichi Kawamura on 4/27/14.
+//  Copyright (c) 2014 Shinichi Kawamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
